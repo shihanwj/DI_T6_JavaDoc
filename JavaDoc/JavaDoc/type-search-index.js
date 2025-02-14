05f0d32d11595e8dffbceeaca3840a8035b04b73
Shihan Wei <shihanwei495@gmail.com>
2025-02-14 10:11:57 +0100

    Delete JavaDoc directory

--- JavaDoc/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"es.ieslosmontecillos.javadoc","l":"HelloApplication"},{"p":"es.ieslosmontecillos.javadoc","l":"Temporizador"}];updateSearchResults();
